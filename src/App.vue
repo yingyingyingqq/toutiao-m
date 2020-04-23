bf4f7e53a32034d4b731d89783e2f039089ff840
buguoqing <1793319032@qq.com>
2020-04-23 13:21:05 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
-    <router-view />
+    <div>
+      <i class="toutiao toutiao-fenxiang"></i>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style scoped lang="less"></style>
